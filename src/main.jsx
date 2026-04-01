2f9d62a1da138b82bfbed814e6c4fd91a0759451
jamesgalovski-lab <james.galovski@gmail.com>
2026-04-01 08:11:38 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NoblDogTracker from "./NoblDogTracker.jsx";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <NoblDogTracker />
+  </React.StrictMode>
+);
